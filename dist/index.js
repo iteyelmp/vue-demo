00e2d7522ca42d4195e9ccaf6ae4ea9b134b8ae7
lmp <iteyelmp@gmail.com>
2020-10-22 13:42:02 +0800

    fix

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+import LedgerBridge from './ledger-bridge';
+
+(async () => {
+	const LB = new LedgerBridge();
+	console.log('LedgerBridge',LB);
+})()
+console.log('QuarkChain < === > Ledger Bridge initialized!')
